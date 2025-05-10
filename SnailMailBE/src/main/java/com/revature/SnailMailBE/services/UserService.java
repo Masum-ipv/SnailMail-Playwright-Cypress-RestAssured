a37a9cd7796d3f4a5e48955d706b7fb2bf669170
Masum-ipv <fakhrulislamcuet@gmail.com>
2025-05-09 21:04:35 -0400

    Added Phase 2 Backend Code

---
+++ SnailMailBE/src/main/java/com/revature/SnailMailBE/services/UserService.java
@@ -0,0 +1,23 @@
+package com.revature.SnailMailBE.services;
+
+import com.revature.SnailMailBE.models.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    //Instantiate the User to send back (hardcoded)
+    User user = new User(
+            "SnailMailGuy123",
+            "me@snailmail.com",
+            "password",
+            "user");
+
+    public User getUser() {
+        return user;
+    }
+
+    public User updateUser(User user) {
+        this.user = user;
+        return user;
+    }
+}
